943191dfbfeeae3b12c97ca431bdabadc1f42fd9
MohamedMahmoud <106127513+MohamedMahmoud20@users.noreply.github.com>
2023-03-20 15:44:48 -0700

    MainScreen

---
+++ lib/Screens/mission_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MissionScreen extends StatelessWidget {
+  const MissionScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      color: Colors.blue,
+    );
+  }
+}
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,6 @@
+import 'package:family_tree/Screens/family_tree.dart';
+import 'package:family_tree/Screens/mission_screen.dart';
+import 'package:family_tree/Screens/setting_screen.dart';
 import 'package:family_tree/app_manager/local_data.dart';
 import 'package:family_tree/providers/language_provider.dart';
 import 'package:family_tree/utilities/text_style.dart';
@@ -14,11 +17,19 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
 
   LanguageProvider? languageProvider;
+  int index=0;
+  List screens=const[
+    MainFamilyTree(),
+    SettingScreen(),
+    MissionScreen()
+  ];
 @override
   void initState() {
   // languageProvider = Provider.of<LanguageProvider>(context, listen: false);
     super.initState();
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -27,114 +38,7 @@ class _MainScreenState extends State<MainScreen> {
 
     return  Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 40.0),
-              child: Center(child: Text(
-                "شجرة الأسرة" ,
-                style: MainTitle.display5(context).copyWith(
-                    fontSize: 28 ,
-                    color:const Color(0xff154C61)),
-              )),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: getSize(context: context).width*0.08),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: horizontalContainer(
-                            image: Icons.my_library_books_outlined,
-                            title: "المكتبه"
-                        ),
-                      ),
-                      verticalContainer(
-                        height:  getSize(context: context).height*0.14,
-                          image: Icons.newspaper,
-                          title: "الاخبار"
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      verticalContainer(
-                        size: 50,
-                        fontSize: 20,
-                          height:  getSize(context: context).height*0.28,
-                          image: Icons.account_box_sharp,
-                          title: "الدليل"
-                      ),
-                      Expanded(
-                        child:
-                      Column(
-                        children: [
-                          horizontalContainer(image: Icons.accessibility, title: "النسب",
-                              size: 60 , fontSize: 25
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(child: verticalContainer(image: Icons.access_alarm_outlined,
-                                  title: "الشخصيات" , size: 45 , fontSize: 18)),
-                              Expanded(child: verticalContainer(image: Icons.newspaper, title: "الشجره", size: 45 , fontSize: 18)),
-                            ],
-                          )
-                        ],
-                      )
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: getSize(context: context).height*0.05,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                designOne(icon: Icons.account_box_sharp , title: "تواصل" , color: mainColor  ,colorTitle:Colors.black),
-                designOne(icon: Icons.access_time_filled_sharp , title: "النادي", color: mainColor,colorTitle:Colors.black),
-                designOne(icon: Icons.add_card_sharp , title: "الصندوق", color: mainColor,colorTitle:Colors.black),
-              ],
-            ),
-            SizedBox(
-              height: getSize(context: context).height*0.05,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 70.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text("احمد", style: MainTitle.display5(context).copyWith(fontSize: 18),),
-                      const SizedBox(height: 5,),
-                      Text("محمود محمد ابراهيم", style: MainTitle.display5(context).copyWith(fontSize: 16)),
-                    ],
-                  ),
-                  const SizedBox(width: 20,),
-                  Expanded(
-                    child: Image.network(
-                        "https://media.istockphoto.com/id/513472966/photo/young-arabian-man-with-arms-crossed.jpg?s=612x612&w=0&k=20&c=r7ifk4l2pZWjjWEq09V1RmFFRdpudyATmCYhfOxFGxs=",
-                      width: 100,
-                      // color: Colors.amber,
-
-                    ),
-                  ),
-
-                ],
-              ),
-            )
-          ],
-        ),
+        child: screens[index]
       ),
       bottomNavigationBar: NavigationBarTheme(
         data: NavigationBarThemeData(
@@ -144,6 +48,11 @@ class _MainScreenState extends State<MainScreen> {
           height: getSize(context: context).height*0.08,
         ),
         child: NavigationBar(
+          onDestinationSelected: (indexx){
+            setState(() {
+              index=indexx;
+            });
+          },
           destinations:const [
             NavigationDestination(icon: Icon(Icons.perm_contact_cal_outlined , color: Colors.white,size: 28,), label: "الهويه",),
             NavigationDestination(icon: Icon(Icons.settings, color: Colors.white,size: 28,), label: "الاعدادات"),
@@ -155,69 +64,5 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  Widget verticalContainer({
-    required String title,
-    required  image,
-      height,
-      width,
-    double? size,
-    double? fontSize,
-  }){
-  return  SizedBox(
-    // padding: EdgeInsets.all(10),
-    height:height??getSize(context: context).height*0.14,
-    width:width?? getSize(context: context).width*0.28,
-    child: Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      color: mainColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-           Icon(image , color: Colors.white,size: size??40,),
-          Text(title, style: WhiteTitle.display5(context).copyWith(fontSize: fontSize??18),),
-        ],
-      ),
-    ),
-  );
-  }
 
-  Widget horizontalContainer({
-    required String title,
-    required  image,
-    double? height,
-    double? size,
-    double? fontSize,
-  }){
-  return  SizedBox(
-    height: height?? getSize(context: context).height*0.14,
-    child: Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      color: mainColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-           Icon(image, color: Colors.white,size: size??45,),
-          Text(title, style: WhiteTitle.display5(context).copyWith(fontSize: fontSize??20.0),),
-        ],
-      ),
-    ),
-  );
-  }
-
-
-  Widget designOne({
-  required String title,
-  required IconData icon,
-    double? size,
-    Color? color,
-    Color? colorTitle,
-    double? fontSize,}){
-  return  Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Icon(icon , color:color??Colors.white,size: size??35),
-      Text(title , style: BlackLabel.display5(context).copyWith(fontSize: fontSize??15 , color: colorTitle??Colors.white),)
-    ],
-  );
-  }
 }
