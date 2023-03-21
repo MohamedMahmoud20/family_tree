a184345a5260f11e0ed703777e07b6ff245d9dae
MoTw1 <babayala50@gmail.com>
2023-03-21 14:29:53 +0200

    .

--- lib/Screens/photo_gallery_screen.dart
+++ lib/Screens/photo_gallery_screen.dart
@@ -18,13 +18,11 @@ class _PhotoGalleryState extends State<PhotoGallery> {
     return  Scaffold(
       appBar: AppBar(),
       body:   SafeArea(
-          child: Column(
+          child: ListView(
             children: [
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 20),
                 alignment: Alignment.centerRight,
-                padding: EdgeInsets.only(
-                    bottom: getSize(context: context).height * 0.03),
                 child: CreatTextField(
                   width: double.infinity,
                   onSubmit: (value){},
@@ -32,31 +30,56 @@ class _PhotoGalleryState extends State<PhotoGallery> {
                   label:"البحث",
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(
-                    bottom: getSize(context: context).height * 0.03),
-                child: Text("2022/02/10" , style: BlackTitle.display5(context),),),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 4,
+                itemBuilder: (BuildContext context, int indexParent) {
+                return  ListView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.only(
+                      top: getSize(context: context).height * 0.02,
+                          bottom: getSize(context: context).height * 0.02),
+                      child: Text("2022/02/1${indexParent}" , style: BlackTitle.display5(context),),),
 
-              Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Image.asset("assets/images/testt.png"),
-                      Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.amber,
-                        ),
-                        height: 50,
-                        child: Text("زيارده جده 1444" , style: WhiteTitle.display5(context),),
-                      )
-                    ],
-                  )
+                    GridView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 6,
+                      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 0.8, crossAxisCount: 3,),
+
+                      itemBuilder: (BuildContext context, int index) {
+                        return  Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            child: Stack(
+                              alignment: Alignment.bottomCenter,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Image.asset("assets/images/testt.png" , fit: BoxFit.cover,height: double.infinity,),
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: mainColor,
+                                  ),
+                                  height: 35,
+                                  child: Text("زياره جده 1444 م " , style: WhiteTitle.display5(context),),
+                                )
+                              ],
+                            )
 
 
-                )
+                        );
+                      },)
+                  ],
+                );
+              },)
             ],
           )
         ),
