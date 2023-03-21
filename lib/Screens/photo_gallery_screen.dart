925cb2e35369e1bf5007faac149541b490ee1d57
MoTw1 <babayala50@gmail.com>
2023-03-21 14:31:28 +0200

    .

--- lib/Screens/photo_gallery_screen.dart
+++ lib/Screens/photo_gallery_screen.dart
@@ -18,69 +18,72 @@ class _PhotoGalleryState extends State<PhotoGallery> {
     return  Scaffold(
       appBar: AppBar(),
       body:   SafeArea(
-          child: ListView(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                alignment: Alignment.centerRight,
-                child: CreatTextField(
-                  width: double.infinity,
-                  onSubmit: (value){},
-                  fillColor: secColor,
-                  label:"البحث",
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+
+            child: ListView(
+              children: [
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: CreatTextField(
+                    width: double.infinity,
+                    onSubmit: (value){},
+                    fillColor: secColor,
+                    label:"البحث",
+                  ),
                 ),
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 4,
-                itemBuilder: (BuildContext context, int indexParent) {
-                return  ListView(
+                ListView.builder(
                   shrinkWrap: true,
                   physics: NeverScrollableScrollPhysics(),
-                  children: [
-                    Container(
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.only(
-                      top: getSize(context: context).height * 0.02,
-                          bottom: getSize(context: context).height * 0.02),
-                      child: Text("2022/02/1${indexParent}" , style: BlackTitle.display5(context),),),
+                  itemCount: 4,
+                  itemBuilder: (BuildContext context, int indexParent) {
+                  return  ListView(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.only(
+                        top: getSize(context: context).height * 0.02,
+                            bottom: getSize(context: context).height * 0.02),
+                        child: Text("2022/02/1${indexParent}" , style: BlackTitle.display5(context),),),
 
-                    GridView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: 6,
-                      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 0.8, crossAxisCount: 3,),
+                      GridView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: 6,
+                        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 0.8, crossAxisCount: 3,),
 
-                      itemBuilder: (BuildContext context, int index) {
-                        return  Card(
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                            child: Stack(
-                              alignment: Alignment.bottomCenter,
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(8),
-                                  child: Image.asset("assets/images/testt.png" , fit: BoxFit.cover,height: double.infinity,),
-                                ),
-                                Container(
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
+                        itemBuilder: (BuildContext context, int index) {
+                          return  Card(
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                              child: Stack(
+                                alignment: Alignment.bottomCenter,
+                                children: [
+                                  ClipRRect(
                                     borderRadius: BorderRadius.circular(8),
-                                    color: mainColor,
+                                    child: Image.asset("assets/images/testt.png" , fit: BoxFit.cover,height: double.infinity,),
                                   ),
-                                  height: 35,
-                                  child: Text("زياره جده 1444 م " , style: WhiteTitle.display5(context),),
-                                )
-                              ],
-                            )
+                                  Container(
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      color: mainColor,
+                                    ),
+                                    height: 35,
+                                    child: Text("زياره جده 1444 م " , style: WhiteTitle.display5(context),),
+                                  )
+                                ],
+                              )
 
 
-                        );
-                      },)
-                  ],
-                );
-              },)
-            ],
+                          );
+                        },)
+                    ],
+                  );
+                },)
+              ],
+            ),
           )
         ),
 
