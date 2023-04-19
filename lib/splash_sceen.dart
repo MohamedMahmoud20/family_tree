17e0f97508dec65003e57524ec3676d0bec9a762
MohamedMahmoud <106127513+MohamedMahmoud20@users.noreply.github.com>
2023-04-19 14:04:13 -0700

    انا جيت

--- lib/splash_sceen.dart
+++ lib/splash_sceen.dart
@@ -2,7 +2,9 @@ import 'package:family_tree/Screens/briths_screens/briths_screen.dart';
 import 'package:family_tree/Screens/guide_screens/guide_screen.dart';
 import 'package:family_tree/Screens/home_screen.dart';
 import 'package:family_tree/Screens/tree_screens/tree_screen.dart';
+import 'package:family_tree/app_manager/local_data.dart';
 import 'package:family_tree/providers/language_provider.dart';
+import 'package:family_tree/utilities/text_style.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,45 +26,65 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
   LanguageProvider? languageProvider;
+//
+late  AnimationController animationControllerParent;
+late  Animation<Offset> animation;
+
 
   @override
   void initState() {
     super.initState();
     SystemChannels.textInput.invokeMethod('TextInput.hide');
     languageProvider = Provider.of<LanguageProvider>(context, listen: false);
-
     languageProvider!.getLanguage();
+
+
+    animationControllerParent=AnimationController(vsync: this , duration: Duration(milliseconds: 200));
+
+    animation=Tween(begin: Offset(0, 2), end: Offset.zero).animate(animationControllerParent);
+animationControllerParent.forward();
+
+Future.delayed(Duration(seconds: 2) ,(){
+    // if ((userId!=null)&&activationCode.isEmpty==true) {
+    //   RoutesManager.navigatorAndRemove(context, MainScreen());
+    // }
+    //
+    // else {
+      RoutesManager.navigatorAndRemove(context, SendConfirmCode());
+  //   }
+
+});
 
 //     String? userId = SharedPreference.getData(key: 'userId');
 //     String activationCode = SharedPreference.getData(key: 'activationCode')??"";
 // // print("splash activationCode : $activationCode");
 //     print("splash UserId : ${userId}");
 
-    Future.delayed(Duration(seconds: 2), () {
-    //   if ((userId!=null)&&activationCode.isEmpty==true) {
-    //     RoutesManager.navigatorAndRemove(context, MainScreen());
-    //   }
-    //
-    //   else {
-        RoutesManager.navigatorAndRemove(context, SendConfirmCode());
-    //   }
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(
-                  "assets/images/splash.png",
-                ),
-                fit: BoxFit.contain)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: getSize(context: context).height*0.4,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                      "assets/images/splash.png",
+                    ),
+                    fit: BoxFit.contain)),
 
+          ),
+          SizedBox(height: 10,),
+         AnimatedBuilder(animation: animation, builder: (context , _)=> SlideTransition(
+             position: animation,
+             child: Text("المقرن" , style: MainTitle.display5(context).copyWith(fontSize: 21),)))
+        ],
       ),
     );
   }
